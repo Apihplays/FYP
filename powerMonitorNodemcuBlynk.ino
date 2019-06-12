35e728e4ce9acdb2837bb2a43af0bbfc64cf0190
Hafiz Mohamed <darkforce95@gmail.com>
2019-06-12 18:44:19 +0800

    update notification system
    now it behaves normally. no more initial notification with 0 reading

--- powerMonitorNodemcuBlynk.ino
+++ powerMonitorNodemcuBlynk.ino
@@ -5,25 +5,23 @@
 SoftwareSerial myS(13,15); //(D8,D7) ==rx,tx pin
 BlynkTimer timer;
 
-char auth[] = "xxx";
-char ssid[] = "xxx";
-char pass[] = "xxx";
+char auth[] = "use yours";
+char ssid[] = "use yours";
+char pass[] = "use yours";
 
 int ledPin1 =  D4;
 int ledState1 = LOW;
 
 int ledPin2 =  LED_BUILTIN;      // the number of the LED pin
 int ledState2 = LOW;             // ledState used to set the LED
-
+int hour = 1;
 float watts,volts,amps,totalEnergy,energyhour;
 
-long period1 = 0;
-long period2 = 5000; //5sec
+unsigned long period1 = 0;
+unsigned long period2 = 5000; //5sec
 
-long period3 = 0;
-long period4 = 3600000; //1 hour 
-
-long hour = 0;
+unsigned long period3 = 0;
+unsigned long period4 = 45000; //1 hour 
 
 unsigned long previousMillis1 = 0;        // will store last time LED was updated
 unsigned long previousMillis2 = 0;        // will store last time LED was updated
@@ -55,7 +53,7 @@ void SerialEvent()
 } 
 
 void DataReceive()
-{   
+{   totalEnergy = ((watts*hour));
     unsigned long currentMillis = millis();
     if((ledState1 == HIGH) && (currentMillis - previousMillis1 >= period1))
     {
@@ -83,30 +81,23 @@ void DataReceive()
     }
      if((ledState2 == HIGH) && (currentMillis - previousMillis2 >= period3))
     {
-    hour = 1;
-    totalEnergy = ((watts*hour));
-    energyhour = energyhour + totalEnergy;
-    
+    energyhour = (totalEnergy*1) + energyhour;
     ledState2 = LOW;  // Turn it off
     previousMillis2 = currentMillis;  // Remember the time
     digitalWrite(ledPin2, ledState2);  // Update the actual LED
-    
     Serial.print("Jam: ");
     Serial.println(hour);
     Serial.print("energy: ");
     Serial.println(energyhour,4);
+    Blynk.virtualWrite(V8,energyhour);
+    Blynk.notify(("The amount of energy usage: ") + String(energyhour) + " Wh");
     }
      else if ((ledState2 == LOW) && (currentMillis - previousMillis2 >= period4)) //after timer reach 1 hour. notification will popup
     {
     ledState2 = HIGH;  // turn it on
     previousMillis2 = currentMillis;   // Remember the time
     digitalWrite(ledPin2, ledState2);    // Update the actual LED
-
-    hour++; //increase the hour by 1 at end of timer reach 1 hour
-    Blynk.virtualWrite(V8,energyhour);
-    Blynk.notify(("The amount of energy usage: ") + String(energyhour) + " Wh");
     }
-    //yield(); //same as loop
 }
 
 void loop() 
